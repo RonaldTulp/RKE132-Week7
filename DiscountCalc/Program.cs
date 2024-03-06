80faee00045b3e2159818826877f251fe1416c73
Ronald <ronald.tulp@tktk.ee>
2024-03-06 22:51:28 +0200

    week7

---
+++ DiscountCalc/Program.cs
@@ -0,0 +1,29 @@
+﻿//arve < 10 - 1%
+//arve >= 10 && arve < 20 - 5%
+//arve >= 20 - 10%
+
+Console.WriteLine("Sisesta arve: ");
+int userInput = Int32.Parse(Console.ReadLine());
+
+double discount = CalculateDiscout(userInput);
+
+Console.WriteLine($"Teie allahindlus on: {discount}");
+
+double newTotal = CalculateNewTotal(userInput, discount);
+
+Console.WriteLine($"Teie uus arve allahindlusega {discount}%: {newTotal}");
+static double CalculateDiscout(int arve)
+{
+    if (arve < 10)
+    { return 1; }
+    else if (arve >= 10 && arve < 20)
+    { return 5; }
+    else
+    { return 10; }
+}
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+}
---
+++ Week7/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in")
+{   PrintHello(); }
+else
+{   PrintGoodBye(); }
+
+PrintHello();
+PrintGoodBye();
+
+static void PrintHello() // Funktsioon / meetod
+{    Console.WriteLine("Hello, world!");}
+
+static void PrintGoodBye()
+{   Console.WriteLine("Adios amigos!"); }
